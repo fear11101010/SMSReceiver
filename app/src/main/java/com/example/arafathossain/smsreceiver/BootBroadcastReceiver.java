369173ccc0284eb26461c04e30e749630ffb8c52
fear11101010 <shuvo11101010@gmail.com>
2015-08-03 09:53:40 +0600

    update

---
+++ app/src/main/java/com/example/arafathossain/smsreceiver/BootBroadcastReceiver.java
@@ -0,0 +1,52 @@
+package com.example.arafathossain.smsreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.util.Log;
+
+
+public class BootBroadcastReceiver extends BroadcastReceiver implements SMSUpload.OnResponseListener {
+    DatabaseHelper helper;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("boot", "complete");
+        helper = new DatabaseHelper(context);
+        if (isNetworkAvailable(context)) {
+            uploadSMS(context);
+        }
+    }
+
+    public boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected() || connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnected();
+    }
+
+    private void uploadSMS(Context context) {
+
+        SMSUpload upload = new SMSUpload(context, this);
+        Cursor cursor = helper.getAllSMS();
+        Log.d("status", cursor.getCount() + "");
+        if (cursor.moveToFirst()) {
+            do {
+                upload.makeRequest(new SMS(cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_ID)), cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_FROM)), cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_BODY)), cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_TIME))));
+                Log.d("ppppp", "oooo");
+            } while (cursor.moveToNext());
+        }
+        helper.deleteAllSMS();
+    }
+
+    @Override
+    public void OnResponseOk(String id) {
+        helper.deleteSMS(id);
+        Log.d("delete", "true");
+    }
+
+    @Override
+    public void OnResponseFailed(SMS sms) {
+
+    }
+}
